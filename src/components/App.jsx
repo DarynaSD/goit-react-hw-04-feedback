fe68b6558cef9d00f6e366aa8ccc918a34b9a9c8
DarynaSD <dasha.dermanchuk@gmail.com>
2023-09-30 13:17:17 +0300

    fix prev

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,13 +18,13 @@ const App = () => {
     //console.log(queryToRenew);
     switch (queryToRenew) {
       case 'good':
-        setGood(good + 1);
+        setGood(prev => prev + 1);
         break;
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prev => prev + 1);
         break;
       case 'bad':
-        setBad(bad + 1);
+        setBad(prev => prev + 1);
         break;
       default:
         alert('click on button!');
